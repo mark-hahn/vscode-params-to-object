55a863c7dd9608a8881c304f58b9da0592ed2e9e
Mark Hahn <mark@hahnca.com>
2025-11-23 13:31:58 -0800

    clnup

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import * as utils  from './utils';
 const { log, start, end } = getLog('cmds');
 
-const outputChannel = vscode.window.createOutputChannel('Data-Scope');
+const outputChannel = vscode.window.createOutputChannel('paramsToObject');
 
 export function getLog(module: string) : {
   log:   (...args: any[]) => void;
